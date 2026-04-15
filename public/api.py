68bf3b111833295bb3d9771e4772b53097721dfc
Triau Nicolas <nicolas.triau@yahoo.fr>
2026-04-15 16:29:55 +0200

    Update: Script API DBNS

--- public/api.py
+++ public/api.py
@@ -1,12 +1,10 @@
-import json
-
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 connected = False
 db = None
 
 try:
-    client = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=5000)
+    client = MongoClient("mongodb://localhost:27017/", serverSelectionTimeoutMS=20000)
     db = client["eslsca-course"]
     connected = True
     print("Connected to MongoDB")
@@ -21,24 +19,60 @@ except ConnectionFailure:
 #     for doc in documents:
 #         print(doc)
 
-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # API Routes
 @app.route("/bonjour", methods=["GET"])
 def say_hello():
-    return jsonify({"status": "success", "result": "bonjour"})
-
+    return jsonify({
+        "status": "success", 
+        "result": "bonjour"
+    })
 
 @app.route("/students", methods=["GET"])
 def get_students():
     try:
         collection = db["students"]
         documents = list(collection.find())
-        return jsonify({"status": "succeess", "reason": "", "result": json.dumps(documents, default=str)})
+        for doc in documents:
+            doc["_id"] = str(doc["_id"])
+
+        return jsonify({
+            "status": "succeess", 
+            "reason": "", 
+            "result": documents
+        })
     except Exception as e:
-        return jsonify({"status": "failed", "reason": str(e), "result": []})
+        return jsonify({
+            "status": "failed", 
+            "reason": str(e), 
+            "result": []
+        })
+
+@app.route("/student", methods=["POST"])
+def add_student():
+    try:
+        collection = db["students"]
+        document = request.get_json()
+        document_to_insert = {
+            "name": document.get("name", ""),
+            "age": int(document.get("age", 0)),
+            "gender": document.get("gender", "")
+        }
+        result = collection.insert_one(document_to_insert)
+
+        return jsonify({
+            "status": "succeess", 
+            "reason": "", 
+            "result": str(document_to_insert)
+        })
+    except Exception as e:
+        return jsonify({
+            "status": "failed", 
+            "reason": str(e), 
+            "result": []
+        })
 
 # Start server
 if __name__ == "__main__":
